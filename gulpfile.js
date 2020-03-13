59bfef4284b810820dfebe8c3010f4cf0e0d8430
arthurwerle <arthur.werle@gmail.com>
2020-03-13 11:30:02 -0300

    gulp now working

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,53 @@
 'use strict';
 const gulp = require('gulp')
+const gutil = require('gulp-util')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
 const sass = require('gulp-sass')
 const cssnano = require('gulp-cssnano')
 const sourcemaps = require('gulp-sourcemaps')
 const source = require('vinyl-source-stream');
 const autoprefixer = require('gulp-autoprefixer')
 
-gulp.task( 'workflow', () => {
+const css = {
+  source: 'src/css',
+  target: 'dist/css'
+}
+
+const js = {
+  source: 'src/js',
+  target: 'dist/js'
+}
+
+gulp.task('css', () => {
     return gulp
-        .src(['./src/sass/**/*.scss'], { base: 'src' } )
-        .pipe( sourcemaps.init() )
-        .pipe( sass().on('error', sass.logError) )
-        .pipe( autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe( cssnano() )
-        .pipe( sourcemaps.write('./') )
-        .pipe( source('build.css') )
-        .pipe( gulp.dest('./dist/css/') )
+      .src([
+          css.source + '/*.css'
+      ])
+      .pipe( sourcemaps.init() )
+      .pipe( sass().on('error', sass.logError) )
+      .pipe( autoprefixer({
+        grid: 'autoplace'
+      }))
+      .pipe( cssnano() )
+      .pipe( sourcemaps.write('./') )
+      .pipe( concat('jupiter.min.css') )
+      .pipe( gulp.dest(css.target) )
 })
 
-gulp.task( 'default', () => {
-    return gulp.watch( './src/sass/**/*.scss', gulp.series('workflow') )
+gulp.task('js', () => {
+  return gulp
+    .src([
+      js.source + '/*.js'
+    ])
+    .pipe( uglify({mangle:true} ).on( 'error', gutil.log ))
+    .pipe( concat('jupiter.min.js') )
+    .pipe( gulp.dest(js.target) )
+})
+
+gulp.task('default', gulp.series(['css', 'js']))
+
+gulp.task('watch', () => {
+  gulp.watch( css.source + '/*.css', ['css'] )
+  gulp.watch( js.source + '/*.js', ['js'] )
 })
