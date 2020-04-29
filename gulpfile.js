e2b85d577034e09666ddaaaa395701879b2c88e7
arthurwerle <arthur.werle@gmail.com>
2020-04-29 15:12:46 -0300

    improving gulp readabillity & identation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
-const gulp = require('gulp')
-const gutil = require('gulp-util')
+const { task, src, dest, series, watch } = require('gulp')
+const { log } = require('gulp-util')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const sass = require('gulp-sass')
@@ -8,40 +8,36 @@ const cssnano = require('gulp-cssnano')
 const autoprefixer = require('gulp-autoprefixer')
 
 const css = {
-    source: './src/sass/main.scss',
-    target: './dist/sass/'
+  source: './src/sass/main.scss',
+  target: './dist/sass/'
 }
 
 const js = {
-    source: 'src/js',
-    target: 'dist/js'
+  source: 'src/js',
+  target: 'dist/js'
 }
   
-gulp.task('css', () => {
-    return gulp
-        .src(css.source)
-        .pipe(sass().on('error', sass.logError))
-        .pipe( autoprefixer({
-            grid: 'autoplace'
-        }))
-        .pipe( cssnano() )
-        .pipe( concat('jupiter.min.css') )
-        .pipe( gulp.dest(css.target) )
-})
+task('css', () => 
+  src(css.source)
+    .pipe(sass().on('error', sass.logError))
+    .pipe( autoprefixer({
+        grid: 'autoplace'
+    }))
+    .pipe(cssnano())
+    .pipe(concat('jupiter.min.css'))
+    .pipe(dest(css.target))
+)
 
-gulp.task('js', () => {
-    return gulp
-        .src([
-          js.source + '/*.js'
-        ])
-        .pipe( uglify({mangle:true} ).on( 'error', gutil.log ))
-        .pipe( concat('jupiter.min.js') )
-        .pipe( gulp.dest(js.target) )
-})
+task('js', () => 
+  src([ js.source + '/*.js' ])
+    .pipe(uglify({ mangle:true } ).on('error', log))
+    .pipe(concat('jupiter.min.js'))
+    .pipe(dest(js.target))
+)
 
-gulp.task('default', gulp.series(['css', 'js']))
+task('default', series(['css', 'js']))
 
-gulp.task('watch', () => {
-    gulp.watch( css.source + '/*.css', gulp.series('css') )
-    gulp.watch( js.source + '/*.js', gulp.series('js') )
+task('watch', () => {
+  watch(css.source + '/*.css', series('css'))
+  watch(js.source + '/*.js', series('js'))
 })
